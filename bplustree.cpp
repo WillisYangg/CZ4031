44fd901cc9b8e7dd2e9b080ab7fc8fcb04e1ee79
WillisYangg <willisyang7@gmail.com>
2023-02-10 20:54:40 +0800

    Add: insert function logic

--- bplustree.cpp
+++ bplustree.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <climits>
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -57,7 +58,100 @@ void BPlusTree::insert(int x)
     }
     else
     {
-
+        Node *cursor = root;
+        Node *parent;
+        //To travel to the leaf node
+        while (cursor->IS_LEAF == false)
+        {
+            parent = cursor;
+            for (int i=0;i<cursor->size;i++)
+            {
+                if (x<cursor->key[i])
+                {
+                    cursor = cursor->ptr[i];
+                    break;
+                }
+                if (i == cursor->size-1)
+                {
+                    cursor = cursor->ptr[i+1];
+                    break;
+                }
+            }
+        }
+        //cursor at leaf node, add nodes to the leaf node
+        if (cursor->size < MAX)
+        {
+            int i = 0;
+            while (x > cursor->key[i] && i < cursor->size)
+            {
+                i++;
+            }
+            for (int j = cursor->size; j>1; j--)
+            {
+                cursor->key[j] = cursor->key[j-1]; //for the new key
+            }
+            cursor->key[i] = x;
+            cursor->size++;
+            cursor->ptr[cursor->size] = cursor->ptr[cursor->size - 1];
+            cursor->ptr[cursor->size - 1] = NULL;
+            cout<<"Inserted "<< x <<" successfully\n";
+        }
+        //create a new leaf node if it is already at the max
+        else
+        {
+            cout<<"Leaf node has reached, creating new leaf node\n";
+            //create new leaf node
+            Node *newLeaf = new Node;
+            int virtualNode[MAX+1];
+            for (int i=0;i<MAX; i++)
+            {
+                virtualNode[i] = cursor->key[i];
+            }
+            int i =0,j;
+            for (int j = MAX + 1; j > i; j--) 
+            {
+                virtualNode[j] = virtualNode[j - 1];
+            }
+            virtualNode[i] = x;
+            newLeaf->IS_LEAF = true;
+            //split the cursor
+            cursor->size = (MAX + 1) / 2;
+            newLeaf->size = MAX + 1 - (MAX + 1) / 2;
+            //cursor point to the new leaf that just got created
+            cursor->ptr[cursor->size] = newLeaf;
+            //new leafNode point to the the new leaf that just got created
+            newLeaf->ptr[newLeaf->size] = cursor->ptr[MAX];
+            cursor->ptr[MAX] = NULL;
+            //fill up the new leaf node
+            for (i=0;i<cursor->size;i++)
+            {
+                cursor->key[i] = virtualNode[i];
+            }
+            for (i=0, j=cursor->size; i<newLeaf->size; i++,j++)
+            {
+                newLeaf->key[i] = virtualNode[j];
+            }
+            //tree only has one node (root node)
+            if (cursor == root)
+            {
+                //create a new root node
+                //tree grows upwards, form new root node
+                Node *newRootNode = new Node();
+                newRootNode->key[0] = newLeaf->key[0];
+                newRootNode->ptr[0] = cursor;
+                newRootNode->ptr[1] = newLeaf;
+                newRootNode->IS_LEAF = false;
+                newRootNode->size = 1;
+                root = newRootNode;
+                cout<<"Inserted new root node successfully\n";
+            }
+            else
+            {
+                //insert new key into parent node
+                insertInternal(newLeaf->key[0], parent, newLeaf);
+                cout<<"Inserted key into parent node successfully\n";
+            }
+        }
     }
 };
 
