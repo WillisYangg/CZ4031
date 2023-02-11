62d19508cbb2edd1bcaab9ce72559d49068d602f
WillisYangg <willisyang7@gmail.com>
2023-02-11 19:23:21 +0800

    Add: insertInternal function logic

--- bplustree.cpp
+++ bplustree.cpp
@@ -153,7 +153,101 @@ void BPlusTree::insert(int x)
             }
         }
     }
-};
+}
+
+//insertInternal operation logic
+void BPlusTree::insertInternal(int x, Node *cursor, Node *child)
+{
+    //cursor is not full
+    if (cursor->size <  MAX)
+    {
+        int i = 0;
+        while (x > cursor->key[i] && i < cursor->size)
+        {
+            i++;
+        }
+        //new key
+        for (int j=cursor->size;j>i;j--)
+        {
+            cursor->key[j] = cursor->key[j-1];
+        }
+        //new pointer
+        for (int j=cursor->size+1;j>i+1;j--)
+        {
+            cursor->ptr[j] = cursor->ptr[j-1];
+        }
+        cursor->key[i] = x;
+        cursor->size++;
+        cursor->ptr[i+1] = child;
+        cout<<"Inserted key into internal node successfully\n";
+    }
+    else
+    {
+        //create new internal node
+        Node *newInternalNode = new Node();
+        int virtualKey[MAX+1];
+        Node *virtualPtr[MAX+2];
+        //new key
+        for (int i=0; i<MAX;i++)
+        {
+            virtualKey[i] = cursor->key[i];
+        }
+        //new pointer
+        for (int i=0; i<MAX+1;i++)
+        {
+            virtualPtr[i] = cursor->ptr[i];
+        }
+        int i=0, j;
+        while (x > virtualKey[i] && i<MAX)
+        {
+            i++;
+        }
+        for (int j = MAX+1; j>i; j--)
+        {
+            virtualKey[j] = virtualKey[j-1];
+        }
+        virtualKey[i] = x;
+        for (int j=MAX+2; j>i; j--)
+        {
+            virtualPtr[j] = virtualPtr[j-1];
+        }
+        virtualPtr[i+1]=child;
+        newInternalNode->IS_LEAF = false;
+        cursor->size = (MAX+1)/2;
+        newInternalNode->size = MAX - (MAX+1)/2;
+        //fill up the new internal node
+        for (i=0, j=cursor->size+1; i<newInternalNode->size; i++,j++)
+        {
+            newInternalNode->key[i] = virtualKey[j];
+        }
+        for (i=0, j=cursor->size+1; i<newInternalNode->size+1; i++,j++)
+        {
+            newInternalNode->ptr[i] = virtualPtr[j];
+        }
+        if (cursor == root)
+        {
+            //create a new root node
+            Node *newRootNode = new Node();
+            newRootNode->key[0] = cursor->key[cursor->size];
+            newRootNode->ptr[0] = cursor;
+            newRootNode->ptr[1] = newInternalNode;
+            newRootNode->IS_LEAF = false;
+            newRootNode->size = 1;
+            root = newRootNode;
+            cout<<"Inserted new root node successfully\n";
+        }
+        else
+        {
+            insertInternal(cursor->key[cursor->size], findParent(root, cursor), newInternalNode);
+        }
+    }
+}
+
+//findParent operation logic
+Node *BPlusTree::findParent(Node* cursor, Node* child)
+{
+    
+}
 
 //getRoot operation logic
 Node *BPlusTree::getRoot()
